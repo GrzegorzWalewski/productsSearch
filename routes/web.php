6f4729837b6d2109f34b8fa16772e5c71a6ab471
Grzojda <grzojda@gmail.com>
2024-05-06 14:25:58 +0200

    init breeze with livewire

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::view('/', 'welcome');
+
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('profile', 'profile')->name('profile');
 });
+
+require __DIR__.'/auth.php';
