da6196123aa3d5d6add4982de376c15af218b2e7
Grzojda <grzojda@gmail.com>
2024-05-17 11:29:58 +0200

    added color for error message

--- resources/views/livewire/import-products.blade.php
+++ resources/views/livewire/import-products.blade.php
@@ -5,7 +5,7 @@
             <div>
                 <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
                 <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" wire:model="file">
-                <div>
+                <div class="p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400">
                     @error('file') <span class="error">{{ $message }}</span> @enderror 
                 </div>
                 <div wire:loading wire:target="file">Uploading...</div>
